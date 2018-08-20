795f7f6ead2895a9565d8af44a1db8f4456bf70a
Maxim A. Klyunnikov <klyunnikov.maksim@gmail.com>
2018-08-21 01:16:54 +0500

    2 commit

--- MailChimp.ps1
+++ MailChimp.ps1
@@ -1,18 +1,17 @@
-﻿#requires -version 5
-#Author:       Maxim A. Klyunnikov
-#Author:       Anatoly S. Demidko
+﻿#requires -version 5.1
+# Link: https://github.com/MaksimKlyu/MailChimp-API.git
+# Author: Maxim A. Klyunnikov
 
 class MailChimp {
-    # Connector Reference: https://docs.microsoft.com/en-us/connectors/mailchimp/
-    
-           [string]         $Version = '2018.08.18'
-           [string]         $UserAgent = "PowerShell/MailChimp-API/3.0 (github.com/MaksimKlyu/MailChimp-API/$( $this.Version ))"
-           [string]         $ApiEndPoint = 'https://<dc>.api.mailchimp.com/3.0/'
-           [int16]          $ApiRequestCount = 10
-           [int16]          $ApiRequestTimeoutSec = 15
-           [PSCustomObject] $ApiRoot
-    hidden [string]         $ApiKey
-    hidden [hashtable]      $Headers
+    [string]           $Version = '2018.08.21'
+    [string]           $UserAgent = "PowerShell/MailChimp-API/3.0 (github.com/MaksimKlyu/MailChimp-API/$( $this.Version ))"
+    [string]           $ApiEndPoint = 'https://<dc>.api.mailchimp.com/3.0/'
+    [int16]            $ApiRequestCount = 10
+    [int16]            $ApiRequestTimeoutSec = 30
+    [string]           $ApiRequestContentType = 'application/json; charset=utf-8'
+    [PSCustomObject]   $ApiRoot #The Experimental Method
+    hidden [string]    $ApiKey
+    hidden [hashtable] $Headers
 
     # Create a new instance
     # @param string $api_key      Your MailChimp API key
@@ -20,129 +19,175 @@ class MailChimp {
     MailChimp( [string]$ApiKey, [string]$ApiEndPoint ) {
         Set-StrictMode -Version Latest
         $ErrorActionPreference = 'Stop'
-        
         if ( $ApiEndPoint.Trim() -eq '' ) {
             $this.ApiEndPoint = $this.ApiEndPoint -replace '<dc>', $this.GetDataCenter($ApiKey)
-        } else {
-            $this.ApiEndPoint = $ApiEndPoint;
         }
-
+        else {
+            $this.ApiEndPoint = $ApiEndPoint
+        }
         $this.SetHeaders($ApiKey)
         
-        #This resource is nothing more than links to other resources available through the API.
-        $this.ApiRoot = $this.InvokeWebRequest( $this.ApiEndPoint, 'GET' )
-	}
-
-    #
-    [string]GetDataCenter( [string]$ApiKey ){
-        if ( $ApiKey -notmatch '^(\w{32})-(us\d{2})$' ) { #TODO: DataCenter [2]
-            throw "Invalid MailChimp API key supplied"
-        }
-        return $Matches[2]
+        # This resource is nothing more than links to other resources available through the API
+        $this.ApiRoot = $this.InvokeGetRequest( $this.ApiEndPoint )
     }
 
-	# Return string The url to the API endpoint
-    [string]GetApiEndpoint(){
-        return $this.ApiEndPoint
+    # Get the Data Center <dc> value from a Mailchimp API key
+    [string]GetDataCenter( [string]$ApiKey ) {
+        if ( $ApiKey -notmatch '^\w{32}-(?<DataCenter>[a-zA-Z]{2}\d{1,2})$' ) {
+            throw "Invalid MailChimp API key supplied"
+        }
+        return $Matches.DataCenter
     }
 
     # Set headers of MailChimp apikey
-    hidden[void]SetHeaders( [string]$ApiKey ){
+    hidden[void]SetHeaders( [string]$ApiKey ) {
         $EncodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($this.Version):$ApiKey"))
         $this.Headers = @{ Authorization = "Basic $EncodedCreds" }
     }
     
-    [void]SetApiRequestCount( [int16]$Count ){
+    [void]SetApiRequestCount( [int16]$Count ) {
         if ($Count -gt 0) {
             $this.ApiRequestCount = $Count
         }
     }
 
-    [void]SetApiRequestTimeoutSec( [int16]$TimeoutSec ){
+    [void]SetApiRequestTimeoutSec( [int16]$TimeoutSec ) {
         if ($TimeoutSec -gt 0) {
             $this.ApiRequestTimeoutSec = $TimeoutSec
         }
     }
 
-    # Convert an email address into a 'subscriber hash' for identifying the subscriber in a method URL
-    # @param  string $email The subscriber's email address
-    # @return string        Hashed version of the input
-	[string]GetSubscriberHash( [string]$Email ){
-		$md5  = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
-		$utf8 = New-Object -TypeName System.Text.UTF8Encoding
-		return [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes( $Email.ToLower() ))).Replace('-', '').ToLower()
+    # Get information about all lists
+    [Object]GetLists() {
+        $Link = $this.ApiEndPoint + "lists"
+        return $this.GetObjects( $Link, 'GET', 'lists', $null )
+    }
+
+    # Get information about all segments in a list
+    [Object]GetListSegments( [string]$ListId ) {
+        $Link = $this.ApiEndPoint + "lists/$ListId/segments"
+        return $this.GetObjects( $Link, 'GET', 'segments', $null )
+    }
+
+    # Get information about a list’s interest categories
+    [Object]GetListInterestCategories( [string]$ListId ) {
+        $Link = $this.ApiEndPoint + "lists/$ListId/interest-categories"
+        return $this.GetObjects( $Link, 'GET', 'categories', $null )
+    }
+
+    # Get information about a list’s interest categories
+    [Object]GetListInterests( [string]$ListId, [string]$InterestCategoryId ) {
+        $Link = $this.ApiEndPoint + "lists/$ListId/interest-categories/$InterestCategoryId/interests"
+        return $this.GetObjects( $Link, 'GET', 'interests', $null )
+    }
+
+    # Get information about members in a list
+    [Object]GetListMembers( [string]$ListId, [string]$Fields ) {
+        $Link = $this.ApiEndPoint + "lists/$ListId/members"
+        return $this.GetObjects( $Link, 'GET', 'members', $Fields )
+    }
+
+    # Add a new list member
+    [Object]AddListMember( [string]$ListId, [hashtable]$Parameters ) {
+        $Link = $this.ApiEndPoint + "lists/$ListId/members"
+        return $this.InvokePostRequest( $Link, $Parameters )
+    }
+
+    # Update a list member
+    [Object]UpdateListMember( [string]$ListId, [hashtable]$Parameters ) {
+        $SubscriberHash = $this.GetSubscriberHash($Parameters.email_address)
+        $Link = $this.ApiEndPoint + "lists/$ListId/members/$SubscriberHash"
+        return $this.InvokePatchRequest( $Link, $Parameters )
     }
     
+    # Remove a list member
+    [Object]RemoveListMember( [string]$ListId, [hashtable]$Parameters ) {
+        $SubscriberHash = $this.GetSubscriberHash($Parameters.email_address)
+        $Link = $this.ApiEndPoint + "lists/$ListId/members/$SubscriberHash"
+        return $this.InvokeDeleteRequest( $Link, $Parameters )
+    }
+    
+
     # Get MailChimp objests
-    [Object]GetObjects( [string]$Uri, [string]$HttpMethod, [string]$PropertyName, [string]$Fields ){
+    [Object]GetObjects( [string]$Uri, [string]$HttpMethod, [string]$ObjectName, [string]$Fields ) {
         $Objects = @()
         $Response = @()
-        $RequestOffset = 0;
+        $RequestOffset = 0
         do {
             $Uri += "?offset=$RequestOffset"
             $Uri += "&count=$( $this.ApiRequestCount )"
             if ( $Fields.Trim() -ne '') {
                 $Uri += '&fields=' + $Fields.Trim() + ',total_items'
             }
-            $Response = $this.InvokeWebRequest( $Uri, $HttpMethod )
-            $Objects += $Response.$( $PropertyName )
+            $Response = $this.InvokeGetRequest( $Uri )
+            $Objects += $Response.$( $ObjectName )
             $RequestOffset += $this.ApiRequestCount
         } while ( $RequestOffset -lt $Response.total_items )
         return $Objects
     }
+
+    # GET request to retrieve data
+    [PSCustomObject]InvokeGetRequest( [string]$Uri ) {
+        try {
+            return Invoke-RestMethod -Uri $Uri -Method Get -Headers $this.Headers -UserAgent $this.UserAgent -TimeoutSec $this.ApiRequestTimeoutSec
+        }
+        catch {
+            throw "Request '$Uri' failed: $( $_.ErrorDetails.Message )"
+        }
+    }
+
+    # POST request to create new resources
+    [PSCustomObject]InvokePostRequest( [string]$Uri, [hashtable]$Body ) {
+        try {
+            $BobyJson = $Body | ConvertTo-Json -Depth 100 -Compress
+            return Invoke-RestMethod -Uri $Uri -Method Post -Headers $this.Headers -Body $BobyJson -UserAgent $this.UserAgent -TimeoutSec $this.ApiRequestTimeoutSec -ContentType $this.ApiRequestContentType
+        }
+        catch {
+            throw "Request '$Uri' failed: $( $_.ErrorDetails.Message )"
+        }  
+    }
+
+    # PATCH request to update a resource
+    [PSCustomObject]InvokePatchRequest( [string]$Uri, [hashtable]$Body ) {
+        try {
+            $BobyJson = $Body | ConvertTo-Json -Depth 100 -Compress
+            return Invoke-RestMethod -Uri $Uri -Method Patch -Headers $this.Headers -Body $BobyJson -UserAgent $this.UserAgent -TimeoutSec $this.ApiRequestTimeoutSec -ContentType $this.ApiRequestContentType
+        }
+        catch {
+            throw "Request '$Uri' failed: $( $_.ErrorDetails.Message )"
+        }
+    }
+
+    # DELETE request to remove a resource [!]
+    [PSCustomObject]InvokeDeleteRequest( [string]$Uri, [hashtable]$Body ) {
+        try {
+            $BobyJson = $Body | ConvertTo-Json -Depth 100 -Compress
+            return Invoke-RestMethod -Uri $Uri -Method Delete -Headers $this.Headers -Body $BobyJson -UserAgent $this.UserAgent -TimeoutSec $this.ApiRequestTimeoutSec -ContentType $this.ApiRequestContentType
+        }
+        catch {
+            throw "Request '$Uri' failed: $( $_.ErrorDetails.Message )"
+        }
+    }
+
+    # Convert an email address into a 'subscriber hash' for identifying the subscriber in a method URL
+    [string]GetSubscriberHash( [string]$Email ) {
+        $Md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+        $Utf8 = New-Object -TypeName System.Text.UTF8Encoding
+        return [System.BitConverter]::ToString($Md5.ComputeHash($Utf8.GetBytes( $Email.ToLower() ))).Replace('-', '').ToLower()
+    }
     
-    #
-    [Object]GetLinkObject ( [PSCustomObject]$AnyObject, [string]$rel ){
-        $link = $AnyObject._links | Where-Object rel -eq $rel
-        if ( -not $link ) {
-            throw "REST API Object Link not found for '$rel'"
+    # The Experimental Method
+    [Object]GetLinkObject ( [PSCustomObject]$AnyObject, [string]$Rel ) {
+        $Link = $AnyObject._links | Where-Object rel -eq $Rel
+        if ( -not $Link ) {
+            throw "REST API Object Link not found. ({1})" -f $AnyObject._links.rel -join ', '
         }
-        return $link
+        return $Link
     }
 
-    # Get information about all lists
-    [Object]GetLists() {
-        $link = $this.GetLinkObject( $this.ApiRoot, 'lists' )
-        return $this.GetObjects( $link.href, $link.method, 'lists', $null )
-    }
-
-    # Get information about a list's segments
-    [Object]GetListSegments( [PSCustomObject]$ListObject ) {
-        $link = $ListObject._links | Where-Object rel -eq 'segments'
-        return $this.GetObjects( $link.href, $link.method, 'segments', $null )
-    }
-
-    # Get information about a list's interest categories
-    [Object]GetListInterestCategories( [PSCustomObject]$ListObject ) {
-        $link = $ListObject._links | Where-Object rel -eq 'interest-categories'
-        return $this.GetObjects( $link.href, $link.method, 'categories', $null )
-    }
-
-    # Get all interests in a specific category
-    [Object]GetListInterests( [PSCustomObject]$InterestCategoryObject ) {
-        $link = $InterestCategoryObject._links | Where-Object rel -eq 'interests'
-        return $this.GetObjects( $link.href, $link.method, 'interests', $null )
-    }
-
-    # Get information about members in a specific MailChimp list
-    [Object]GetListMembers( [PSCustomObject]$ListObject, [string]$Fields ) {
-        $link = $this.GetLinkObject( $ListObject, 'members' )
-        return $this.GetObjects( $link.href, $link.method, 'members', $fields )
-    }
-
-    [PSCustomObject]InvokeWebRequest( [string]$Uri, [string]$HttpMethod ){
-        try {
-            return Invoke-WebRequest -Uri $Uri -Method $HttpMethod -Headers $this.Headers -UserAgent $this.UserAgent -TimeoutSec $this.ApiRequestTimeoutSec | ConvertFrom-Json
-        } catch {
-            throw "Request '$Uri' failed: $( $_.ErrorDetails.Message )"
-        }
-    }
-
-    [PSCustomObject]InvokeRestMethod( [string]$Uri, [string]$HttpMethod ){
-        try {
-            return Invoke-RestMethod -Uri $Uri -Method $HttpMethod -Headers $this.Headers -UserAgent $this.UserAgent -TimeoutSec $this.ApiRequestTimeoutSec
-        } catch {
-            throw "Request '$Uri' failed: $( $_.ErrorDetails.Message )"
-        }
+    # The Experimental Method
+    [Object]GetCustomObjets( [PSCustomObject]$CustomObject, [string]$rel, [string]$ObjectName ) {
+        $Link = $this.GetLinkObject( $CustomObject, $Rel )
+        return $this.GetObjects( $Link.href, $Link.method, $ObjectName, $null )
     }
 }
